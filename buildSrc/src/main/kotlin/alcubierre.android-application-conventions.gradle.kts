ed69f684a794c6db8eaf31c556b2165663ede816
Alexander Mitropolsky <aam.dev@fastmail.com>
2023-10-07 15:04:27 +0300

    AGP 8

--- buildSrc/src/main/kotlin/alcubierre.android-application-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/alcubierre.android-application-conventions.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.github.octaone.alcubierre_sample"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
