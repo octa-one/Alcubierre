8a785a27611ecd87de9768cfb042f4d4b0e7cceb
Alexander Mitropolsky <aam.dev@fastmail.com>
2024-05-06 21:25:08 +0400

    KDOC

--- alcubierre-base/src/main/kotlin/com/github/octaone/alcubierre/reduce/LinkedNavReducer.kt
+++ alcubierre-base/src/main/kotlin/com/github/octaone/alcubierre/reduce/LinkedNavReducer.kt
@@ -6,11 +6,11 @@ import com.github.octaone.alcubierre.annotation.AlcubierreInternalApi
  * Linked list implementation of [NavReducer].
  * The reduce starts at the head and traverses to the tail.
  *
- * Each reducer has access to the head to have an ability ti restart the entire chain.
+ * Each reducer has access to the head to have an ability to restart the entire chain.
  * As an example, there's a chain:
  * HeadReducer -> ForwardReducer -> DeeplinkReducer
  * DeeplinkReducer waits for an action with the deeplink, processes it and triggers a chain of reducers with Forward action.
- * But after DeeplinkReducer, there are no more reducers for Forward reducing.
+ * But there are no more reducers in the downstream, so, Forward reducing is impossible.
  * This is the case when it is safer to restart the chain from the head with a new action.
  */
 public abstract class LinkedNavReducer<S> : NavReducer<S> {
